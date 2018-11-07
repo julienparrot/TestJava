0a1400b997b1482eb7bbb36b44665a8627a4f5a8
jparrot <julien.parrot91@hotmail.fr>
2018-11-07 08:35:27 +0100

    TEST ECLIPSE COMMIT
    *Commentaire

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Fenetre maFenetre= new Fenetre();
+		//TP_1 monTP_1 = new TP_1();
+		//TP_2 monTP_2 = new TP_2();
+		//TP_3 monTP_3 = new TP_3();
+		//TP_4 monTP_4 = new TP_4();
+		//TP_5 monTP_5 = new TP_5();
+		//TP_6 monTP_6 = new TP_6();
+		Pokedex monPokedex = new Pokedex();
+		//Supprimer unesuppression = new Supprimer();
+		//Ajouter unAjout = new Ajouter();
+		 //Recherche unerecherche = new Recherche();
+		//tabPokemon2 untab = new tabPokemon2();
+		//test 
+	}
+
+}
